d15cbc64b25355b272a309d2860ddfb0967df535
Chigozirim Ibeabuchi [el16cai] <el16cai@eentpc98.leeds.ac.uk>
2017-09-28 09:44:45 +0100

    Initial Commit

---
+++ Lab1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab1
+//
+//  Created by Chigozirim Ibeabuchi [el16cai] on 28/09/2017.
+//  Copyright © 2017 Chigozirim Ibeabuchi [el16cai]. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
